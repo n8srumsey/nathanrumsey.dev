21a9e070b2ca7b4342adb7f21f7898b4e90bec0b
Nathan Rumsey <nathan@nathanrumsey.dev>
2026-05-14 20:36:20 -0700

    chore(utils): remove deprecated filterSort utility
    filterSort logic has been replaced by dedicated blogFilters and projectFilters modules with better separation of concerns.

--- src/utils/filterSort.ts
+++
@@ -1,5 +0,0 @@
-export function navigate(search: string): void {
-  const url = search ? `${window.location.pathname}?${search}` : window.location.pathname;
-  history.pushState(null, '', url);
-  window.dispatchEvent(new PopStateEvent('popstate'));
-}
